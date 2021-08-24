624a6e2c4cbb7759cc0706473cf61a930fac908b
n0noob <neemanoop@gmail.com>
2021-08-24 17:30:32 +0530

    Added backtracking algorithms

---
+++ src/test/java/com/n0noob/backtracking/KnightTourTest.java
@@ -0,0 +1,23 @@
+package com.n0noob.backtracking;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KnightTourTest {
+
+    KnightTour knightTour;
+
+    @BeforeEach
+    void setUp() {
+        knightTour = new KnightTour();
+    }
+
+    @Test
+    public void givenStdBoard_whenSolveAttempted_thenShouldSolve() {
+        boolean res = knightTour.solve(7, 7);
+        assertTrue(res);
+    }
+
+}
